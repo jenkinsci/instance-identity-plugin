e35b10148b8c8d41910ea8fd09b93cd99d69516b
Jesse Glick <jglick@cloudbees.com>
2020-11-03 18:03:26 -0500

    Code cleanups in SelfSignedCertificate

--- src/main/java/org/jenkinsci/main/modules/instance_identity/SelfSignedCertificate.java
+++ src/main/java/org/jenkinsci/main/modules/instance_identity/SelfSignedCertificate.java
@@ -9,11 +9,9 @@ import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
-import java.security.interfaces.DSAParams;
 import java.security.interfaces.DSAPrivateKey;
 import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.RSAPrivateKey;
-import java.security.spec.ECParameterSpec;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
@@ -21,18 +19,13 @@ import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x500.X500NameBuilder;
 import org.bouncycastle.asn1.x500.style.BCStyle;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.Extension;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
-import org.bouncycastle.asn1.x509.X509Extension;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
-import org.bouncycastle.crypto.params.DSAKeyParameters;
-import org.bouncycastle.crypto.params.DSAParameters;
-import org.bouncycastle.crypto.params.ECDomainParameters;
-import org.bouncycastle.crypto.params.ECKeyParameters;
 import org.bouncycastle.crypto.params.RSAKeyParameters;
 import org.bouncycastle.jcajce.provider.asymmetric.dsa.DSAUtil;
 import org.bouncycastle.jcajce.provider.asymmetric.util.ECUtil;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
 import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
@@ -40,10 +33,9 @@ import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.operator.bc.BcDSAContentSignerBuilder;
 import org.bouncycastle.operator.bc.BcECContentSignerBuilder;
 import org.bouncycastle.operator.bc.BcRSAContentSignerBuilder;
-import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 
-public final class SelfSignedCertificate {
-    private static final BouncyCastleProvider BOUNCY_CASTLE_PROVIDER = new BouncyCastleProvider();
+final class SelfSignedCertificate {
+
     private final KeyPair keyPair;
     private Date firstDate = new Date();
     private Date lastDate = new Date(firstDate.getTime() + TimeUnit.DAYS.toMillis(365));
@@ -137,7 +129,7 @@ public final class SelfSignedCertificate {
 
             JcaX509ExtensionUtils instance = new JcaX509ExtensionUtils();
 
-            certGen.addExtension(X509Extension.subjectKeyIdentifier,
+            certGen.addExtension(Extension.subjectKeyIdentifier,
                     false,
                     instance.createSubjectKeyIdentifier(subjectPublicKeyInfo)
             );
